691c564aa1671afd7e430dc1f636d1809c71c279
Kevin Irías <kirias@cmnhospitals.org>
2021-11-29 17:05:08 -0600

    Added misc images examples

---
+++ images/octo-deploy-windows/scripts/update_path.cmd
@@ -0,0 +1,1 @@
+setx /M path "%PATH%;C:\Users\ContainerAdministrator\AppData\Local\Microsoft\dotnet;C:\Program Files\PackageManagement\NuGet\Packages\Octopus.Client.8.4.2\lib\net452\Octopus.Client.dll"
